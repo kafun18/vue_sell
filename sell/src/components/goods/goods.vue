7d5d985e3d2c996b3c37e3e65283bff5382e38f2
kafun <513112374@qq.com>
2019-04-25 18:31:58 +0800

    布局

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="header">
-    我是header
-  </div>
+<div>i am goods</div>
 </template>
 
 <script type="text/ecmascript-6">
--- sell/src/main.js
+++ sell/src/main.js
@@ -1,15 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+// import router from './router';
+import VueRouter from 'vue-router';
+import goods from './components/goods/goods';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
+// let app = Vue.extend(App);
+// let router = new VueRouter();
+// router.map({
+//   '/goods': {
+//     component: goods
+//   }
+//   });
+// router.start(app,'#app');
+
+const routes = [
+  {path: '/goods', component: goods}
+];
+const router = new VueRouter({
+  routes
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+});
